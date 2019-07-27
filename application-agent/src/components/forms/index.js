d0f3ab3fe14e9ee1c00cb6db9558ed0c6018b7a3
GuyARoss <guyallenross@gmail.com>
2019-07-26 22:34:39 -0400

    added register user from.

--- application-agent/src/components/forms/index.js
+++ application-agent/src/components/forms/index.js
@@ -1,1 +1,2 @@
 export { default as RegisterClient } from './register_client_form';
+export { default as RegisterUser } from './register_user_form';
---
+++ application-agent/src/components/forms/register_user_form.jsx
@@ -0,0 +1,47 @@
+import React, { useCallback } from 'react';
+
+import { Input, Typography, Form } from 'antd';
+
+export default ({ onChange, error, loading }) => {
+    const onFieldChange = useCallback(({ target }) => {
+        onChange(target)
+    }, [onChange]);
+
+    return (
+        <>
+            <Typography.Title level={4}>
+                Create your account
+            </Typography.Title>
+
+            <div className="input_group">
+                <div className="input_container">
+                    <Form.Item
+                        validateStatus={error['username'].status}
+                        help={error['username'].help}
+                    >
+                        <Typography.Text strong>Username</Typography.Text>
+                        <Input id="username" onChange={onFieldChange} />
+                    </Form.Item>
+                </div>
+                <div className="input_container">
+                    <Form.Item
+                        validateStatus={error['email'].status}
+                        help={error['email'].help}
+                    >
+                        <Typography.Text strong>Email Address</Typography.Text>
+                        <Input id="email" onChange={onFieldChange} />
+                    </Form.Item>
+                </div>
+                <div className="input_container">
+                    <Form.Item
+                        validateStatus={error['password'].status}
+                        help={error['password'].help}
+                    >
+                        <Typography.Text strong>Password</Typography.Text>
+                        <Input.Password id="password" onChange={onFieldChange} />
+                    </Form.Item>
+                </div>
+            </div>
+        </>
+    )
+}
