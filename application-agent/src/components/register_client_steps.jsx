d41b66a81c74bc7a4d92b8796a98a886e15eb935
GuyARoss <guyallenross@gmail.com>
2019-07-26 22:31:50 -0400

    added account type to steps.

--- application-agent/src/components/register_client_steps.jsx
+++ application-agent/src/components/register_client_steps.jsx
@@ -4,8 +4,8 @@ import { Steps, Icon } from 'antd';
 
 export default ({ current, loading }) => (
     <Steps current={current}>
-        <Steps.Step title="Create User" description="Create a basic user account" />
-        {/* <Steps.Step title="Choose Account Type" /> */}
-        <Steps.Step title="Finish" icon={loading ? <Icon type="loading" /> : null}/>
+        <Steps.Step title="Create User" description="Create a basic user account"  icon={loading && current === 0 ? <Icon type="loading" /> : null}/>
+        <Steps.Step title="Choose Account Type"  icon={loading && current === 1? <Icon type="loading" /> : null}/>
+        <Steps.Step title="Finish" icon={loading && current === 2 ? <Icon type="loading" /> : null}/>
     </Steps>
 )
