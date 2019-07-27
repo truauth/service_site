32cbd07ac3ac7cb0ef1da73fa4d451627588f3e5
GuyARoss <guyallenross@gmail.com>
2019-07-27 01:10:33 -0400

    added account settings.

--- application-agent/src/components/forms/index.js
+++ application-agent/src/components/forms/index.js
@@ -1,2 +1,3 @@
 export { default as RegisterClient } from './register_client_form';
 export { default as RegisterUser } from './register_user_form';
+export { default as AccountSettings } from './account_settings_form';
