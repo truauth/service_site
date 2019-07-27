2804f380d64f3ca479fe8d5c2a72bd5c7624f14d
GuyARoss <guyallenross@gmail.com>
2019-07-26 22:35:04 -0400

    added rest of content for register client form.

--- application-agent/src/components/forms/register_client_form.jsx
+++ application-agent/src/components/forms/register_client_form.jsx
@@ -1,32 +1,59 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 
-import { Input, Typography } from 'antd';
+import { Input, Typography, Switch } from 'antd';
 
 export default ({ onChange }) => {
     const onFieldChange = useCallback(({ target }) => {
         onChange(target)
     }, [onChange]);
 
+    const [showGroup, setShowGroup] = useState(false);
+    const toggleSwitch = () => {
+        setShowGroup(!showGroup)
+    }
+
     return (
         <>
             <Typography.Title level={4}>
-                Create your account
+                Choose Account Settings
             </Typography.Title>
-    
-            <div className="input_group">
-                <div className="input_container">
-                    <Typography.Text strong>Username</Typography.Text>
-                    <Input id="username" onChange={onFieldChange}/>
-                </div>
-                <div className="input_container">
-                    <Typography.Text strong>Email Address</Typography.Text>
-                    <Input id="email" onChange={onFieldChange}/>
-                </div>
-                <div className="input_container">
-                    <Typography.Text strong>Password</Typography.Text>
-                    <Input.Password id="password" onChange={onFieldChange}/>
+            <div className="input_container">
+                <Typography.Text strong style={{ marginRight: 10 }}>Developer Account</Typography.Text>
+                <Switch onChange={toggleSwitch} />
+                <div>
+                    <Typography.Text style={{ fontSize: 10 }}>
+                        Enable the developer account for integrating your applications with truauth, if you do not plan to integrate, do not select this option.
+                        You can select this option later, if you choose to. 
+                    </Typography.Text>
                 </div>
             </div>
+            {
+                showGroup && (
+                    <>
+                        <Typography.Title level={4} style={{ marginTop: 10 }}>Register Client</Typography.Title>
+
+                        <div className="input_group">
+                            <div className="input_container">
+                                <Typography.Text strong>Name</Typography.Text>
+                                <Input id="name" onChange={onFieldChange} />
+                                <div>
+                                    <Typography.Text style={{ fontSize: 10 }}>
+                                        Name of the client utilizing truauth
+                                    </Typography.Text>
+                                </div>
+                            </div>
+                            <div className="input_container">
+                                <Typography.Text strong>Callback Uri</Typography.Text>
+                                <Input id="callbackURI" onChange={onFieldChange} />
+                            </div>
+                            <div className="input_container">
+                                <Typography.Text strong>Redirect URI</Typography.Text>
+                                <Input id="redirectURI" onChange={onFieldChange} />
+                            </div>
+                        </div>
+                    </>
+                )
+            }
         </>
     )
 }
