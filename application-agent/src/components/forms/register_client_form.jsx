1d2cb3ba56d1269c2ae070a1c4f117dee4c162d6
GuyARoss <guyallenross@gmail.com>
2019-07-27 01:10:51 -0400

    moved accounts settings acount of client form.

--- application-agent/src/components/forms/register_client_form.jsx
+++ application-agent/src/components/forms/register_client_form.jsx
@@ -1,59 +1,50 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback} from 'react';
 
-import { Input, Typography, Switch } from 'antd';
+import { Input, Typography, Form } from 'antd';
 
-export default ({ onChange }) => {
+export default ({ onChange, error }) => {
     const onFieldChange = useCallback(({ target }) => {
         onChange(target)
     }, [onChange]);
 
-    const [showGroup, setShowGroup] = useState(false);
-    const toggleSwitch = () => {
-        setShowGroup(!showGroup)
-    }
-
     return (
         <>
-            <Typography.Title level={4}>
-                Choose Account Settings
-            </Typography.Title>
-            <div className="input_container">
-                <Typography.Text strong style={{ marginRight: 10 }}>Developer Account</Typography.Text>
-                <Switch onChange={toggleSwitch} />
-                <div>
-                    <Typography.Text style={{ fontSize: 10 }}>
-                        Enable the developer account for integrating your applications with truauth, if you do not plan to integrate, do not select this option.
-                        You can select this option later, if you choose to. 
-                    </Typography.Text>
+            <Typography.Title level={4} style={{ marginTop: 10 }}>Register Client</Typography.Title>
+
+            <div className="input_group">
+                <div className="input_container">
+                    <Form.Item
+                        validateStatus={error['clientName'].status}
+                        help={error['clientName'].help}
+                    >
+                        <Typography.Text strong>Name</Typography.Text>
+                        <Input id="name" onChange={onFieldChange} />
+                        <div>
+                            <Typography.Text style={{ fontSize: 10 }}>
+                                Name of the client utilizing truauth
+                                        </Typography.Text>
+                        </div>
+                    </Form.Item>
+                </div>
+                <div className="input_container">
+                    <Form.Item
+                        validateStatus={error['callbackURI'].status}
+                        help={error['callbackURI'].help}
+                    >
+                        <Typography.Text strong>Callback Uri</Typography.Text>
+                        <Input id="callbackURI" onChange={onFieldChange} />
+                    </Form.Item>
+                </div>
+                <div className="input_container">
+                    <Form.Item
+                        validateStatus={error['redirectURI'].status}
+                        help={error['redirectURI'].help}
+                    >
+                        <Typography.Text strong>Redirect URI</Typography.Text>
+                        <Input id="redirectURI" onChange={onFieldChange} />
+                    </Form.Item>
                 </div>
             </div>
-            {
-                showGroup && (
-                    <>
-                        <Typography.Title level={4} style={{ marginTop: 10 }}>Register Client</Typography.Title>
-
-                        <div className="input_group">
-                            <div className="input_container">
-                                <Typography.Text strong>Name</Typography.Text>
-                                <Input id="name" onChange={onFieldChange} />
-                                <div>
-                                    <Typography.Text style={{ fontSize: 10 }}>
-                                        Name of the client utilizing truauth
-                                    </Typography.Text>
-                                </div>
-                            </div>
-                            <div className="input_container">
-                                <Typography.Text strong>Callback Uri</Typography.Text>
-                                <Input id="callbackURI" onChange={onFieldChange} />
-                            </div>
-                            <div className="input_container">
-                                <Typography.Text strong>Redirect URI</Typography.Text>
-                                <Input id="redirectURI" onChange={onFieldChange} />
-                            </div>
-                        </div>
-                    </>
-                )
-            }
         </>
     )
 }
