32cfac89de7d3f5f04480426bff7a9b15fe060b7
GuyARoss <guyallenross@gmail.com>
2019-07-28 00:27:14 -0400

    removed register client

--- application-agent/src/screens/index.js
+++ application-agent/src/screens/index.js
@@ -1,3 +1,2 @@
 export { default as Home } from './home';
-export { default as RegiserClient } from './register_client/register_client';
-export { default as RegisterUser } from './register_user';
+export { default as RegisterUser } from './register_user/screen';
